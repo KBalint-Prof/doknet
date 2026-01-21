c938d5d6de1f7d74d04e664cfc584bee59c3d739
KBalint-Prof <kbalintsuli@gmail.com>
2026-01-21 11:16:19 +0100

    editing

---
+++ src/app/news/[id]/Edit.tsx
@@ -0,0 +1,186 @@
+'use client';
+
+import { useContext, useState } from 'react';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import { GlobalContext } from '../../context/GlobalContext';
+
+const TinyMCEEditor = dynamic(
+  () => import('@tinymce/tinymce-react').then((mod) => mod.Editor),
+  { ssr: false },
+);
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+};
+
+export default function Edit({ open, onClose }: Props) {
+  if (!open) return null;
+
+  const [content, setContent] = useState('');
+  const [saving, setSaving] = useState(false);
+  const [message, setMessage] = useState('');
+  const [title, setTitle] = useState('');
+  const [selectedCover, setSelectedCover] = useState<string | null>(null);
+  const [uploadedCover, setUploadedCover] = useState<string | null>(null);
+  const defaultCovers = [
+    '/covers/bejelentes.png',
+    '/covers/felhivas.png',
+    '/covers/fontos.png',
+    '/covers/szavazas.png',
+  ];
+  const router = useRouter();
+  const ctx = useContext(GlobalContext);
+
+  console.log('MENTÉS:', {
+    title,
+    content,
+    selectedCover,
+  });
+  const handleSave = async () => {
+    setSaving(true);
+    setMessage('');
+
+    try {
+      console.log(ctx?.user?.id);
+      const res = await fetch('/api/news', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify({
+          title,
+          content,
+          cover_img: selectedCover,
+          user_id: ctx?.user?.id,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok)
+        throw new Error(data.error || 'Hiba történt a mentés során.');
+
+      setMessage(`Sikeres mentés! (ID: ${data.id})`);
+      router.push('/');
+    } catch (err: any) {
+      console.error(err);
+      setMessage('Hiba a mentés során!');
+    } finally {
+      setSaving(false);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        inset: 0,
+        background: 'rgba(0,0,0,0.5)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 1000,
+      }}
+      onClick={onClose}
+    >
+      <div
+        style={{
+          background: '#fff',
+          padding: '1.5rem',
+          borderRadius: '8px',
+          minWidth: '300px',
+          minHeight: '150px',
+        }}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <h3>Szerkesztés</h3>
+
+        <input
+          style={{
+            width: '97%',
+            padding: '0.7rem 0.9rem',
+            marginBottom: '1rem',
+          }}
+          type="text"
+          placeholder="Hír címe"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        {
+          <TinyMCEEditor
+            apiKey={process.env.NEXT_PUBLIC_EDITOR_KEY}
+            init={{
+              language: 'hu_HU',
+              language_url: '/hu_HU.js',
+              height: 500,
+              resize: false,
+              entity_encoding: 'raw',
+              menubar: true,
+              plugins: [
+                'advlist',
+                'autolink',
+                'lists',
+                'link',
+                'image',
+                'charmap',
+                'preview',
+                'anchor',
+                'searchreplace',
+                'visualblocks',
+                'code',
+                'fullscreen',
+                'insertdatetime',
+                'media',
+                'table',
+                'help',
+                'wordcount',
+              ],
+              toolbar:
+                'undo redo | blocks | bold italic underline | alignleft aligncenter alignright alignjustify | ' +
+                'bullist numlist outdent indent | image media link | removeformat | help',
+              images_upload_handler: (blobInfo: any) =>
+                new Promise((resolve) => {
+                  const base64 =
+                    'data:' +
+                    blobInfo.blob().type +
+                    ';base64,' +
+                    blobInfo.base64();
+                  resolve(base64);
+                }),
+              automatic_uploads: true,
+              file_picker_types: 'image',
+              image_class_list: [
+                { title: 'None', value: '' },
+                { title: 'Float left (wrap text)', value: 'img-left' },
+                { title: 'Float right (wrap text)', value: 'img-right' },
+                { title: 'Centered', value: 'img-center' },
+              ],
+              image_dimensions: true,
+              content_style: `
+          body {
+            font-family: Arial, sans-serif;
+            font-size: 16px;
+          }
+            img { max-width: 100%; height: auto; }
+            .img-left { float: left; margin: 1rem; }
+            .img-right { float: right; margin: 1rem; }
+            .img-center { display: block; margin: 1rem auto; float: none; }
+          `,
+              placeholder: 'Írd ide a hír tartalmát...',
+            }}
+          />
+        }
+
+        <button onClick={onClose} style={{ marginTop: '1rem' }}>
+          Bezárás
+        </button>
+        <button onClick={handleSave} disabled={saving}>
+          {saving ? 'Mentés folyamatban...' : 'Mentés'}
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -1,9 +1,10 @@
-"use client";
+'use client';
 
-import Comments from "@/app/news/[id]/Comments";
-import Reactions from "@/app/news/[id]/Reactions";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import Comments from '@/app/news/[id]/Comments';
+import Reactions from '@/app/news/[id]/Reactions';
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import Edit from '@/app/news/[id]/Edit';
 
 export interface NewsType {
   id: number;
@@ -38,6 +39,8 @@ export default function NewsPage() {
   const params = useParams<{ id: string }>();
   const id = Number(params.id);
 
+  const [isEditOpen, setIsEditOpen] = useState(false);
+
   const [news, setNews] = useState<NewsType | null>(null);
   const [reactionTypes, setReactionTypes] = useState<ReactionTypeType[]>([]);
 
@@ -46,10 +49,10 @@ export default function NewsPage() {
       const result = await fetch(`/api/news/${id}`);
       const data = await result.json();
 
-      console.log("NEWS BY ID:", data);
+      console.log('NEWS BY ID:', data);
 
       if (!result.ok || data.news.length === 0)
-        throw new Error(data.error || "Hiba történt a hír betöltése során.");
+        throw new Error(data.error || 'Hiba történt a hír betöltése során.');
 
       setNews(data.news[0]);
 
@@ -64,69 +67,69 @@ export default function NewsPage() {
   }, [id]);
 
   return (
-    <main style={{ padding: "2rem" }}>
+    <main style={{ padding: '2rem' }}>
       {!news && <div>Betöltés...</div>}
 
       {news && (
         <>
           <div
             style={{
-              width: "100%",
-              display: "flex",
-              justifyContent: "center",
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
             }}
           >
             <img
-              src={news.cover_img ?? ""}
+              src={news.cover_img ?? ''}
               alt="Borítókép"
               style={{
-                width: "100%",
-                maxWidth: "700px",
-                height: "25rem",
-                objectFit: "cover",
-                borderRadius: "8px",
+                width: '100%',
+                maxWidth: '700px',
+                height: '25rem',
+                objectFit: 'cover',
+                borderRadius: '8px',
               }}
             />
           </div>
           <div
             style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "1rem",
-              marginBottom: "1rem",
+              display: 'flex',
+              alignItems: 'center',
+              gap: '1rem',
+              marginBottom: '1rem',
             }}
           >
-            <h1 style={{ fontFamily: "Arial", margin: 0 }}>{news.title}</h1>
+            <h1 style={{ fontFamily: 'Arial', margin: 0 }}>{news.title}</h1>
 
-            <button>Szerkesztés</button>
+            <button onClick={() => setIsEditOpen(true)}>Szerkesztés</button>
           </div>
 
           <article
             className="news-content"
             dangerouslySetInnerHTML={{ __html: news.content }}
-            style={{ marginTop: "1rem", fontSize: "1.2rem", lineHeight: "1.6" }}
+            style={{ marginTop: '1rem', fontSize: '1.2rem', lineHeight: '1.6' }}
           ></article>
           <small
             style={{
-              marginTop: "0.75rem",
-              color: "#777",
-              display: "flex",
-              flexDirection: "column",
-              gap: "0.25rem",
+              marginTop: '0.75rem',
+              color: '#777',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '0.25rem',
               lineHeight: 1.4,
             }}
           >
             <span>
-              Közzétéve:{" "}
+              Közzétéve:{' '}
               <time dateTime={news.created_at}>
-                {new Date(news.created_at).toLocaleString("hu-HU")}
+                {new Date(news.created_at).toLocaleString('hu-HU')}
               </time>
             </span>
 
             <span>
-              Közzétette:{" "}
+              Közzétette:{' '}
               <strong style={{ fontWeight: 500 }}>
-                {news.author_name ?? "Ismeretlen"}
+                {news.author_name ?? 'Ismeretlen'}
               </strong>
             </span>
           </small>
@@ -138,6 +141,7 @@ export default function NewsPage() {
           <Comments news={news} getNewsById={getNewsById} />
         </>
       )}
+      <Edit open={isEditOpen} onClose={() => setIsEditOpen(false)} />
     </main>
   );
 }
