8555a88b81a48334d903accd30d519584833c493
Gyemike77_ <matekarbyqk@gmail.com>
2026-01-28 09:19:57 +0100

    gallery2

--- src/lib/db.ts
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise';
-export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '', 
-  database: 'doknetdb', // Ez a fontos!
-  waitForConnections: true,
-  connectionLimit: 10,
-});
