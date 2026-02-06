7a2ed0f3975f05856f79f6ddd8f4aeeaf52be611
KBalint-Prof <kevesba@gmail.com>
2026-02-06 07:37:44 +0100

    törölt hírek korlátozása

--- src/app/news-editor/[id]/page.tsx
+++ src/app/news-editor/[id]/page.tsx
@@ -2,9 +2,27 @@
 
 import NewsEditor from "@/app/components/NewsEditor";
 import { useParams } from "next/navigation";
+import { GlobalContext } from "../../context/GlobalContext";
+import { useContext } from "react";
+
+const allowed_roles = ["admin", "teacher", "president"];
 
 export default function NewsPage() {
   const params = useParams<{ id: string }>();
   const id = Number(params.id);
+  const ctx = useContext(GlobalContext);
+
+  if (!ctx?.user || !allowed_roles.includes((ctx.user as any).role)) {
+    return (
+      <>
+        <div style={{ padding: "2rem", color: "red" }}>
+          Nincs jogosultságod a hírszerkesztő használatához.
+        </div>
+        <button onClick={() => (window.location.href = "/")}>
+          Vissza a főoldalra
+        </button>
+      </>
+    );
+  }
   return <NewsEditor id={id} />;
 }
